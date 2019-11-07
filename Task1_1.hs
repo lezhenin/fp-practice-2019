cd3495861daf7bbfbc66db37121a3d25fca19767
Iurii Lezhenin <yurij.lezhenin@gmail.com>
2019-11-07 16:11:58 +0300

    implement Task1

--- Task1_1.hs
+++ Task1_1.hs
@@ -7,26 +7,51 @@ module Task1_1 where
 
 import Todo(todo)
 
+data Op = Add
+          | Substract
+          | Multiply
+          deriving(Show,Eq)
+
 data Term = IntConstant{ intValue :: Int }           -- числовая константа
             | Variable{ varName :: String }          -- переменная
-            | BinaryTerm{ lhv :: Term, rhv :: Term } -- бинарная операция
+            | BinaryTerm{ op :: Op, lhv :: Term, rhv :: Term } -- бинарная операция
             deriving(Show,Eq)
 
 -- Для бинарных операций необходима не только реализация, но и адекватные
 -- ассоциативность и приоритет
+
 (|+|) :: Term -> Term -> Term
-(|+|) l r = todo
+(|+|) (IntConstant l) (IntConstant r) = IntConstant (l + r)
+(|+|) l r                             = BinaryTerm Add l r
+
 (|-|) :: Term -> Term -> Term
-(|-|) l r = todo
+(|-|) (IntConstant l) (IntConstant r) = IntConstant (l - r)
+(|-|) l r                             = BinaryTerm Substract l r
+
 (|*|) :: Term -> Term -> Term
-(|*|) l r = todo
+(|*|) (IntConstant l) (IntConstant r) = IntConstant (l * r)
+(|*|) l r                             = BinaryTerm Multiply l r
+
+infixl 6 |+|
+infixl 6 |-|
+infixl 7 |*|
 
 -- Заменить переменную `varName` на `replacement`
 -- во всём выражении `expression`
 replaceVar :: String -> Term -> Term -> Term
-replaceVar varName replacement expression = todo
+replaceVar varName replacement constant@(IntConstant _) = constant
+replaceVar varName replacement varibale@(Variable name) | varName == name = replacement
+                                                        | otherwise = varibale 
+replaceVar varName replacement (BinaryTerm op lhv rhv)  = BinaryTerm op newLhv newRhv
+    where newLhv = replaceVar varName replacement lhv
+          newRhv = replaceVar varName replacement rhv
 
 -- Посчитать значение выражения `Term`
 -- если оно состоит только из констант
 evaluate :: Term -> Term
-evaluate expression = todo
+evaluate const@(IntConstant _)          = const
+evaluate (BinaryTerm Add lhv rhv)       = (evaluate lhv) |+| (evaluate rhv)
+evaluate (BinaryTerm Substract lhv rhv) = (evaluate lhv) |-| (evaluate rhv)
+evaluate (BinaryTerm Multiply lhv rhv)  = (evaluate lhv) |*| (evaluate rhv)
+evaluate _                              = error "Non constant term"                                                                         
+
--- Task1_2.hs
+++ Task1_2.hs
@@ -9,8 +9,13 @@ import Todo(todo)
 
 -- синус числа (формула Тейлора)
 sin :: Double -> Double
-sin x = todo
-
+sin x = sum (take 10 terms)
+    where terms = map (taylor_sin angle) [0,1..]
+          angle = (fmod (x + pi) (2 * pi)) - pi
+          fmod a b = a - (fromIntegral $ truncate (a / b)) * b
+          taylor_sin y n = ((-1) ** n) * (y ** k) / (product [1..k])
+              where k = 2 * n + 1
+       
 -- косинус числа (формула Тейлора)
 cos :: Double -> Double
 cos x = todo
@@ -31,19 +36,30 @@ isDateCorrect day month year = todo
 -- возведение числа в степень, duh
 -- готовые функции и плавающую арифметику использовать нельзя
 pow :: Integer -> Integer -> Integer
-pow x y = todo
-
+pow x 0 = 1
+pow x 1 = x
+pow x y | even y = pow (x * x) (div y 2)
+        | odd  y = x * pow (x * x) (div y 2)
+                
 -- является ли данное число простым?
 isPrime :: Integer -> Bool
-isPrime x = todo
-
+isPrime x | x < 4     = x > 1 
+          | otherwise = null [k | k <- [2..(isqrt x)], (mod x k) == 0]
+    where isqrt n = test n
+              where test x | x * x > n = test(x - 1)
+                           | otherwise = x
+                
 type Point2D = (Double, Double)
 
 -- рассчитайте площадь многоугольника по формуле Гаусса
 -- многоугольник задан списком координат
 shapeArea :: [Point2D] -> Double
-shapeArea points = todo
-
+shapeArea points = 0.5 * abs (sumOfProducts xs (shift ys) - sumOfProducts (shift xs) ys) 
+    where sumOfProducts a b = sum $ (zipWith (*)) a b 
+          shift (h:t) = t ++ [h]
+          xs = map fst points
+          ys = map snd points
+          
 -- треугольник задан своими координатами.
 -- функция должна вернуть
 --  0, если он тупоугольный
